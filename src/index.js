2031cc78106206aa3fbbf74a89f8e72f0ca900be
Camila Mendez <mcmendezmanotas@gmail.com>
2024-05-06 12:13:25 -0500

    creando la funcion createNewTodo para crear una nueva tarea

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import "./css/base.css";
 
-import { pageState } from "./js/utils";
+import { pageState, createNewTodo } from "./js/utils";
+import { newTodo } from "./js/node";
 
 pageState();
+// Agregar evento de escucha para crear una nueva tarea al presionar Enter
+newTodo.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    createNewTodo();
+  }
+});
--- src/js/utils.js
+++ src/js/utils.js
@@ -5,8 +5,10 @@ import {
   nodeTodoCount,
   completeButton,
   nodeFilters,
-} from "./store.js";
+  newTodo,
+} from "./node.js";
 
+//1. Ocultar las secciones main y footer
 export function pageState(display = false) {
   const initialState = JSON.parse(localStorage.getItem("mydayapp-js")) || [];
 
@@ -17,4 +19,33 @@ export function pageState(display = false) {
     nodeFooter.classList.add("hidden");
     nodeMain.classList.add("hidden");
   }
+}
+
+//2. Crear una nueva tarea
+export function createNewTodo() {
+  const inputValue = newTodo.value.trim();
+
+  if (inputValue !== "") {
+    const todoItem = document.createElement("li");
+    todoItem.innerHTML = `
+      <div class="view">
+        <input class="toggle" type="checkbox">
+        <label>${inputValue}</label>
+        <button class="destroy"></button>
+      </div>
+      <input class="edit" value="${inputValue}">
+    `;
+    todoList.appendChild(todoItem);
+
+    newTodo.value = "";
+
+    saveDataToLocalStorage();
+  }
+}
+
+function saveDataToLocalStorage() {
+  const tasks = Array.from(todoList.children).map(
+    (todo) => todo.querySelector("label").textContent
+  );
+  localStorage.setItem("mydayapp-js", JSON.stringify(tasks));
 }
