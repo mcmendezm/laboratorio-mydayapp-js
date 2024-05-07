4c6405b5e61e6de2bbd86fae5fa6efa1108af057
Camila Mendez <mcmendezmanotas@gmail.com>
2024-05-06 19:30:03 -0500

    haciendo los eventos de los li para cumplir con el objetivo de  Editando una tarea

--- src/js/utils.js
+++ src/js/utils.js
@@ -12,7 +12,7 @@ import {
 export function pageState(display = false) {
   const initialState = JSON.parse(localStorage.getItem("mydayapp-js")) || [];
 
-  if (initialState.length > 0) {
+  if (initialState.length > 1) {
     nodeFooter.classList.remove("hidden");
     nodeMain.classList.remove("hidden");
   } else if (display) {
@@ -40,12 +40,59 @@ export function createNewTodo() {
     newTodo.value = "";
 
     saveDataToLocalStorage();
+    addTaskEvents(todoItem);
   }
 }
 
+//3. Una tarea y 4.  Editando una tarea
+export function addTaskEvents(task) {
+  const checkbox = task.querySelector(".toggle");
+  const label = task.querySelector("label");
+  const destroyButton = task.querySelector(".destroy");
+  const editInput = task.querySelector(".edit");
+
+  checkbox.addEventListener("click", function () {
+    task.classList.toggle("completed");
+    saveDataToLocalStorage();
+  });
+  label.addEventListener("dblclick", function () {
+    task.classList.add("editing");
+    editInput.focus();
+  });
+  task.addEventListener("mouseenter", function () {
+    destroyButton.style.display = "block";
+  });
+
+  task.addEventListener("mouseleave", function () {
+    destroyButton.style.display = "none";
+  });
+
+  destroyButton.addEventListener("click", function () {
+    task.remove();
+    saveDataToLocalStorage();
+  });
+
+  editInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      label.textContent = editInput.value.trim();
+      task.classList.remove("editing");
+      saveDataToLocalStorage();
+    }
+  });
+  editInput.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      editInput.value = label.textContent;
+      task.classList.remove("editing");
+    }
+  });
+}
+
 function saveDataToLocalStorage() {
-  const tasks = Array.from(todoList.children).map(
-    (todo) => todo.querySelector("label").textContent
-  );
+  const tasks = Array.from(todoList.children).map((todo) => {
+    return {
+      content: todo.querySelector("label").textContent,
+      completed: todo.classList.contains("completed"),
+    };
+  });
   localStorage.setItem("mydayapp-js", JSON.stringify(tasks));
 }
